48b17300fcdec09e8441100f52cf5a8d623e1fce
hamstar0 <webutek@gmail.com>
2019-10-30 14:38:34 -0700

    v0.2.3 - Added auto alerts to viable houses (WIP) * Added auto alerts to viable houses (WIP) * Restructured house furnishing kit code

---
+++ Items/HouseFurnishingKitItem_Validate.cs
@@ -0,0 +1,99 @@
+﻿using HamstarHelpers.Classes.Tiles.TilePattern;
+using HamstarHelpers.Helpers.Tiles;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using System;
+
+
+namespace AdventureMode.Items {
+	partial class HouseFurnishingKitItem : ModItem {
+		public static bool IsValidHouse( int tileX, int tileY ) {
+			IList<(ushort TileX, ushort TileY)> innerHouseSpace;
+			IList<(ushort TileX, ushort TileY)> fullHouseSpace;
+			int floorY;
+
+			return HouseFurnishingKitItem.IsValidHouse( tileX, tileY, out innerHouseSpace, out fullHouseSpace, out floorY );
+		}
+
+
+		public static bool IsValidHouse(
+					int tileX,
+					int tileY,
+					out IList<(ushort TileX, ushort TileY)> innerHouseSpace,
+					out IList<(ushort TileX, ushort TileY)> fullHouseSpace,
+					out int floorY ) {
+			bool customCheck( int x, int y ) {
+				Tile tile = Main.tile[x, y];
+				return !tile.active()
+					|| ( !Main.tileSolid[tile.type] && !Main.tileSolidTop[tile.type] )  //non-solid, non-platform
+					|| ( Main.tileSolid[tile.type] && Main.tileSolidTop[tile.type] && tile.slope() != 0 );  //stair
+			}
+
+			TilePattern pattern1 = new TilePattern( new TilePatternBuilder {
+				AreaFromCenter = new Rectangle( -2, -2, 4, 4 ),
+				HasWater = false,
+				HasHoney = false,
+				HasLava = false,
+				IsActuated = false,
+				CustomCheck = customCheck
+			} );
+			TilePattern pattern2 = new TilePattern( new TilePatternBuilder {
+				HasWater = false,
+				HasHoney = false,
+				HasLava = false,
+				IsActuated = false,
+				CustomCheck = customCheck
+			} );
+
+			if( !HouseFurnishingKitItem.IsValidHouseByCriteria( pattern1, tileX, tileY, out innerHouseSpace, out floorY ) ) {
+				fullHouseSpace = innerHouseSpace;
+				return false;
+			}
+
+			if( !HouseFurnishingKitItem.IsValidHouseByCriteria( pattern2, tileX, tileY, out fullHouseSpace, out floorY ) ) {
+				return false;
+			}
+
+			return true;
+		}
+
+
+		private static bool IsValidHouseByCriteria(
+					TilePattern pattern,
+					int tileX,
+					int tileY,
+					out IList<(ushort TileX, ushort TileY)> houseSpace,
+					out int floorY ) {
+			IList<(ushort TileX, ushort TileY)> unclosedTiles;
+			houseSpace = TileFinderHelpers.GetAllContiguousMatchingTiles(
+				pattern,
+				tileX,
+				tileY,
+				out unclosedTiles,
+				64
+			);
+
+			if( unclosedTiles.Count > 0 ) {
+				Main.NewText( "House too large or not a closed space.", Color.Yellow );
+				floorY = 0;
+				return false;
+			}
+
+			if( houseSpace.Count < 80 ) {
+				Main.NewText( "House too small.", Color.Yellow );
+				floorY = 0;
+				return false;
+			}
+
+			int floorWidth = TileFinderHelpers.GetFloorWidth( pattern, tileX, tileY - 2, 32, out floorY );
+			if( floorWidth < 10 ) {
+				Main.NewText( "Not enough floor space.", Color.Yellow );
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ MyPlayer.cs
@@ -0,0 +1,88 @@
+﻿using AdventureMode.Items;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+
+namespace AdventureMode {
+	class AdventureModePlayer : ModPlayer {
+		private int CurrentZoomedX;
+		private int CurrentZoomedY;
+		private bool PreviousHouseCheck = false;
+
+
+		////////////////
+
+		public override bool CloneNewInstances => false;
+
+
+
+		////////////////
+
+		public override void PreUpdate() {
+			if( Main.netMode != 2 ) {
+				this.PreUpdateLocal();
+			}
+		}
+		
+		private void PreUpdateLocal() {
+			if( Main.myPlayer != this.player.whoAmI ) {
+				return;
+			}
+
+			int zoomedX = (int)this.player.Center.X / 64;
+			int zoomedY = (int)this.player.Center.Y / 64;
+
+			if( this.CurrentZoomedX == 0 ) {
+				this.CurrentZoomedX = zoomedX;
+				this.CurrentZoomedY = zoomedY;
+			} else {
+				if( this.CurrentZoomedX != zoomedX || this.CurrentZoomedY != zoomedY ) {
+					this.CurrentZoomedX = zoomedX;
+					this.CurrentZoomedY = zoomedY;
+
+					this.UpdateViableHouseCheck();
+				}
+			}
+		}
+
+		////
+
+		private void UpdateViableHouseCheck() {
+			bool? isViable = this.CheckIfViableHouseIfNearby();
+
+			if( isViable.HasValue && isViable.Value ) {
+				if( !this.PreviousHouseCheck ) {
+					this.PreviousHouseCheck = true;
+
+					Main.NewText( "Valid town house space found. Note: Only above ground houses work automatically.", Color.Lime );
+				} else {
+					this.PreviousHouseCheck = false;
+				}
+			} else {
+				this.PreviousHouseCheck = false;
+			}
+		}
+
+
+		////////////////
+
+		public bool? CheckIfViableHouseIfNearby() {
+			int tileX = (int)this.player.Center.X / 16;
+			int tileY = (int)this.player.Center.Y / 16;
+			int houseKitItemType = ModContent.ItemType<HouseFurnishingKitItem>();
+
+			for( int i = 0; i < this.player.inventory.Length; i++ ) {
+				Item item = this.player.inventory[i];
+				if( item == null || item.IsAir || item.type != houseKitItemType ) {
+					continue;
+				}
+
+				return HouseFurnishingKitItem.IsValidHouse( tileX, tileY );
+			}
+
+			return null;
+		}
+	}
+}
