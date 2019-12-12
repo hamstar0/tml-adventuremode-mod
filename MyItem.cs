4201a27ac22315df4b92472ae07b77d8ba21a1de
hamstar0 <webutek@gmail.com>
2019-12-11 22:46:16 -0800

    v0.8.2 - Moved mana crystal decrease to FindableManaCrystals mod

--- MyItem.cs
+++ MyItem.cs
@@ -65,12 +65,12 @@ namespace AdventureMode {
 		
 		public override void OnConsumeItem( Item item, Player player ) {
 			switch( item.type ) {
-			case ItemID.ManaCrystal:
+			/*case ItemID.ManaCrystal:
 				if( AdventureModeConfig.Instance.ReducedManaCrystalStatIncrease ) {
 					player.statManaMax -= 15;
 				}
 				this.ModifyPopupText();
-				break;
+				break;*/	//<- Implemented via FindableManaCrystals mod
 			case ItemID.LifeCrystal:
 				if( AdventureModeConfig.Instance.ReducedLifeCrystalStatIncrease ) {
 					player.statLifeMax -= 15;
