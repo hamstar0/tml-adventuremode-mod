9e8ec31bcf6f7504c43a5ba4fb078aa469a20f7d
hamstar0 <webutek@gmail.com>
2021-06-06 21:11:22 -0700

    v0.25.0 - Fixed broken default spawn picker

--- AdventureMode/WorldGeneration/Gen_Spawn.cs
+++ AdventureMode/WorldGeneration/Gen_Spawn.cs
@@ -8,6 +8,17 @@ using ModLibsGeneral.Libraries.World;
 
 namespace AdventureMode.WorldGeneration {
 	partial class AMWorldGen {
+		public static void SetBeachSpawn( GenerationProgress progress ) {
+			bool foundNewSpawn = AMWorldGen.FindBeachSpawn( progress, out int tileX, out int tileY );
+
+			if( foundNewSpawn ) {
+				AMWorldGen.SetSpawn( tileX, tileY - 2 );
+			}
+		}
+
+
+		////
+
 		private static void SetSpawn( int x, int y ) {
 			var myworld = ModContent.GetInstance<AMWorld>();
 
@@ -21,18 +32,20 @@ namespace AdventureMode.WorldGeneration {
 		}
 
 
-		////
+		////////////////
 
-		public static void SetBeachSpawn( GenerationProgress progress ) {
+		private static bool FindBeachSpawn( GenerationProgress progress, out int tileX, out int tileY ) {
 			int skyBot = WorldLocationLibraries.SkyLayerBottomTileY;
+			int surfBot = WorldLocationLibraries.SurfaceLayerBottomTileY;
 
 			bool checkColumns( int myTileX, out int myTileY ) {
-				for( myTileY = skyBot; myTileY < skyBot; myTileY++ ) {
+				for( myTileY = skyBot; myTileY < surfBot; myTileY++ ) {
 					Tile tile = Framing.GetTileSafely( myTileX, myTileY );
 					if( tile == null || !tile.active() ) {
 						continue;
 					}
-					if( Main.tile[myTileX, myTileY - 1].liquid != 0 ) {
+
+					if( Framing.GetTileSafely(myTileX, myTileY - 1).liquid != 0 ) {
 						break;
 					}
 
@@ -43,19 +56,17 @@ namespace AdventureMode.WorldGeneration {
 
 			//
 
-			bool foundNewSpawn = false;
 			int reach = 40;//340;
-			int tileX, tileY=0;
+			tileY = 0;
 
 			if( Main.dungeonX > (Main.maxTilesX / 2) ) {
 				int max = (Main.maxTilesX - reach) - Main.dungeonX;
 
 				for( tileX = Main.maxTilesX - reach; tileX > Main.dungeonX; tileX-- ) {
-					progress.Value = (float)(tileX - Main.dungeonX) / (float)max;
-					foundNewSpawn = checkColumns( tileX, out tileY );
+					progress.Value = (float)( tileX - Main.dungeonX ) / (float)max;
 
-					if( foundNewSpawn ) {
-						break;
+					if( checkColumns(tileX, out tileY) ) {
+						return true;
 					}
 				}
 			} else {
@@ -63,17 +74,14 @@ namespace AdventureMode.WorldGeneration {
 
 				for( tileX = reach; tileX < max; tileX++ ) {
 					progress.Value = (float)( tileX - reach ) / (float)max;
-					foundNewSpawn = checkColumns( tileX, out tileY );
-
-					if( foundNewSpawn ) {
-						break;
+					
+					if( checkColumns(tileX, out tileY) ) {
+						return true;
 					}
 				}
 			}
 
-			if( foundNewSpawn ) {
-				AMWorldGen.SetSpawn( tileX, tileY - 2 );
-			}
+			return false;
 		}
 	}
 }
