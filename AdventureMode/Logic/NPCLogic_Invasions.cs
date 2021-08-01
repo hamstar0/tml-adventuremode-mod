d7b55a7ec94fa3960c59df8208a882f4f8428768
hamstar0 <webutek@gmail.com>
2021-08-01 03:50:59 -0700

    tweaks

--- AdventureMode/Logic/NPCLogic_Invasions.cs
+++ AdventureMode/Logic/NPCLogic_Invasions.cs
@@ -1,21 +1,27 @@
 ﻿using System;
 using Terraria;
+using Terraria.ID;
 using ModLibsGeneral.Libraries.NPCs;
 using ModLibsGeneral.Libraries.World;
 
 
 namespace AdventureMode.Logic {
 	static partial class NPCLogic {
-		public static void SetInvasionDefaults( NPC npc ) {
+		/*public static void SetInvasionDefaults( NPC npc ) {
 			//DebugLibraries.Print( "invasionstate", "invasionSize:"+Main.invasionSize
 			//	+", invasionProgress:"+Main.invasionProgress
 			//	+", invasionProgressMax:"+Main.invasionProgressMax
 			//	+", invasionType:"+Main.invasionType
 			//	+", y:"+(npc.position.Y < WorldLibraries.DirtLayerTopTileY)
 			//	+", wet:"+npc.wet);
-			if( npc.position.Y < (WorldLocationLibraries.DirtLayerTopTileY * 16) && npc.wet ) {
-				NPCLibraries.Remove( npc );
+
+			// Is above ground
+			if( npc.position.Y < (WorldLocationLibraries.DirtLayerTopTileY * 16) ) {
+				// Is wet
+				if( npc.wet ) {
+					NPCLibraries.Remove( npc, Main.netMode == NetmodeID.Server );
+				}
 			}
-		}
+		}*/
 	}
 }
