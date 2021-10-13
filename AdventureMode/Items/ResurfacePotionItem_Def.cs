44fb3e76a54fb91327ce8ae58222a746bf8a426f
hamstar0 <webutek@gmail.com>
2021-10-13 08:04:37 -0700

    v0.32.0 - Renamed/restructured item code files

---
+++ AdventureMode/Items/ResurfacePotionItem_Def.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ModLibsGeneral.Libraries.Players;
+using ModLibsGeneral.Libraries.World;
+using AdventureMode.Recipes;
+
+
+namespace AdventureMode.Items {
+	public partial class ResurfacePotionItem : ModItem {
+		public static void CheckItemForPlayer( Player player, Item item ) {
+			if( player.itemAnimation <= 0 ) {
+				return;
+			}
+
+			if( player.itemTime == 0 ) {
+				player.itemTime = PlayerHooks.TotalUseTime( item.useTime, player, item );
+			} else if( player.itemTime == 2 ) {
+				ResurfacePotionItem.ApplyWarp( player, item );
+			}
+		}
+
+
+
+		////////////////
+
+		public override void SetStaticDefaults() {
+			this.DisplayName.SetDefault( "Resurface Potion" );
+			this.Tooltip.SetDefault(
+				"Warps you to your last sight of daylight/moonlight"
+				+"\nDoes not work if too deep"
+			);
+		}
+		
+		public override void SetDefaults() {
+			this.item.width = 14;
+			this.item.height = 24;
+
+			this.item.maxStack = 30;
+
+			this.item.value = Item.buyPrice( silver: 5 );
+			this.item.rare = ItemRarityID.Blue;
+
+			this.item.consumable = true;
+			this.item.potion = true; // Makes it so this item applies potion sickness on use and allows it to be used with quick heal
+			
+			this.item.useStyle = ItemUseStyleID.EatingUsing;
+			this.item.useAnimation = 17;
+			this.item.useTime = 17;
+			this.item.useTurn = true;
+
+			this.item.UseSound = SoundID.Item3;
+		}
+
+		public override void AddRecipes() {
+			var recipe = new ResurfacePotionRecipe( this );
+			recipe.AddRecipe();
+		}
+
+
+		////////////////
+
+		public override bool ConsumeItem( Player player ) {
+			if( !ResurfacePotionItem.CanResurface(player, out string msg) ) {
+				Main.NewText( msg, Color.Yellow );
+				return false;
+			}
+			return true;
+		}
+	}
+}
--- AdventureMode/Items/ResurfacePotionItem_Warp.cs
+++ AdventureMode/Items/ResurfacePotionItem_Warp.cs
@@ -8,22 +8,7 @@ using AdventureMode.Recipes;
 
 
 namespace AdventureMode.Items {
-	public class ResurfacePotionItem : ModItem {
-		public static void CheckItemForPlayer( Player player, Item item ) {
-			if( player.itemAnimation <= 0 ) {
-				return;
-			}
-
-			if( player.itemTime == 0 ) {
-				player.itemTime = PlayerHooks.TotalUseTime( item.useTime, player, item );
-			} else if( player.itemTime == 2 ) {
-				ResurfacePotionItem.ApplyWarp( player, item );
-			}
-		}
-
-
-		////////////////
-		
+	public partial class ResurfacePotionItem : ModItem {
 		public static bool CanResurface( Player player, out string result ) {
 			int tileX = (int)player.MountedCenter.X / 16;
 			int tileY = (int)player.MountedCenter.Y / 16;
@@ -111,54 +96,6 @@ namespace AdventureMode.Items {
 			if( ItemLoader.ConsumeItem(item, player) && item.stack > 0 ) {
 				item.stack--;
 			}
-		}
-
-
-
-		////////////////
-
-		public override void SetStaticDefaults() {
-			this.DisplayName.SetDefault( "Resurface Potion" );
-			this.Tooltip.SetDefault(
-				"Warps you to your last sight of daylight/moonlight"
-				+"\nDoes not work if too deep"
-			);
-		}
-		
-		public override void SetDefaults() {
-			this.item.width = 14;
-			this.item.height = 24;
-
-			this.item.maxStack = 30;
-
-			this.item.value = Item.buyPrice( silver: 5 );
-			this.item.rare = ItemRarityID.Blue;
-
-			this.item.consumable = true;
-			this.item.potion = true; // Makes it so this item applies potion sickness on use and allows it to be used with quick heal
-			
-			this.item.useStyle = ItemUseStyleID.EatingUsing;
-			this.item.useAnimation = 17;
-			this.item.useTime = 17;
-			this.item.useTurn = true;
-
-			this.item.UseSound = SoundID.Item3;
-		}
-
-		public override void AddRecipes() {
-			var recipe = new ResurfacePotionRecipe( this );
-			recipe.AddRecipe();
-		}
-
-
-		////////////////
-
-		public override bool ConsumeItem( Player player ) {
-			if( !ResurfacePotionItem.CanResurface(player, out string msg) ) {
-				Main.NewText( msg, Color.Yellow );
-				return false;
-			}
-			return true;
 		}
 	}
 }
